11bcab48ea20171d1caaed9ed4da8c6103e1c109
ronik <ronik.carkey@gmail.com>
2023-09-01 16:42:47 +1000

    added src files and notebooks

--- src/data/encoders.py
+++ src/data/encoders.py
@@ -1,37 +1,8 @@
-import pandas as pd
-import numpy as np
-
-def map_months(date):
-    month_dict = {
-    'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
-    'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12
-    }
-    split_value = date.split('-')
-    if len(split_value)==2:
-        if (split_value[0] in month_dict):
-            feet = month_dict.get(split_value[0],1)
-            inch = 0
-            return feet, inch
-            
-        elif(split_value[1] in month_dict):
-            feet = month_dict.get(split_value[1],1)
-            inch = split_value[0]
-            return feet, inch
-        
-        else:
-            return 0, 0
-    else:
-        return 0, 0
-    
-        
-def convert_height_to_cm(df):
-    ft, inch = map_months(df)
-    cm = (int(ft) * 30.48) + (int(inch) * 2.54) 
-    if cm == 0:
-        return np.nan
-    return cm
-
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import pandas as pd
+
+# Add train data and test data in the encoder
+# Encoder is performed only on train data and uses trained encoder in the test data
 
 def perform_label_encoding(train_data, test_data, list_col_name):
     for col in list_col_name:
@@ -48,5 +19,7 @@ def perform_one_hot_encoding(train_data, test_data, to_encode):
     test_ohe = ohe.transform(test_data[to_encode]).toarray()
     encoded_test_data = pd.DataFrame(test_ohe, index=test_data.index, columns=ohe.get_feature_names_out(to_encode))
     test_data_enc = pd.merge(test_data, encoded_test_data, left_index=True, right_index=True)
-    
+    train_data_enc = train_data_enc.drop(to_encode, axis=1)
+    test_data_enc = test_data_enc.drop(to_encode, axis=1)
+
     return train_data_enc, test_data_enc
